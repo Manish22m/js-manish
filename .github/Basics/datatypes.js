2b4407bebf16f93d71bc99a555cb2f76f8221b3f
Manish22m <140962031+Manish22m@users.noreply.github.com>
2025-01-19 10:56:11 +0000

    variable conversion

---
+++ .github/Basics/datatypes.js
@@ -0,0 +1,13 @@
+//let no="22";
+//let num=Number(no);
+//console.log(num);
+//console.log(typeof (num));
+
+// let number=10;
+// let string=String(number);
+// console.log(string);
+
+let no=true;
+let num=Number(no);
+console.log(num);
+console.log(typeof(num));
