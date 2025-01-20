72aa45d8618457c0e32b1b9e0cb6ca1f2679ec9a
Manish22m <140962031+Manish22m@users.noreply.github.com>
2025-01-20 11:46:13 +0000

    array

---
+++ .github/Basics/arrays.js
@@ -0,0 +1,14 @@
+const arr1=[1,2,3,4,5,6];
+console.log(arr1[3]);
+
+// array methods
+arr1.push(7);
+arr1.push(8);
+// console.log(arr1.toString())
+// console.log(typeof(arr1))
+
+console.log(arr1)
+const arr2=arr1.slice(2,4);
+console.log(arr2);
+const arr3=arr1.splice(2,4);
+console.log(arr3)
