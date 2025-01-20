e1fa4aa330cf86e635a59ebf86db8132556b27a5
Manish22m <140962031+Manish22m@users.noreply.github.com>
2025-01-20 06:38:13 +0000

    string methods

---
+++ .github/Basics/string.js
@@ -0,0 +1,10 @@
+let naav='manish mengane'
+const age=22;
+console.log(`My naav is ${naav} aand I am ${age} years old`);
+console.log(naav.length);
+console.log(naav.charAt(4))
+console.log(naav.indexOf('g'))
+console.log(naav.replace(' ', '-'))
+console.log(naav.split(2, 5))
+
+
