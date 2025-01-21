ef0caa069237d967ff7b60a975267d1d7f43c780
Manish22m <140962031+Manish22m@users.noreply.github.com>
2025-01-21 14:52:05 +0000

    invoked functions

---
+++ Basics2/invoked.js
@@ -0,0 +1,11 @@
+(function execute(){
+    // console.log('manish');
+})();
+
+// in above first () represent whole function and another () represent execution part
+
+(function fruit(fruitname){
+    // console.log(`fruit is ${fruitname}`)
+})('apple');
+
+console.log( ((num) => (`number is ${num}` ) ) ( 2 ) );
