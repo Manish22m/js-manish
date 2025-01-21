fc807ecbe01db497d8790bd6e7c82b0cca7619aa
Manish22m <140962031+Manish22m@users.noreply.github.com>
2025-01-21 08:14:18 +0000

    object and json talk

---
+++ .github/Basics/objects.js
@@ -0,0 +1,13 @@
+const user={
+    name:'manish',
+    id:123,
+    company:'abs'
+}
+console.log(user.id);
+
+// destructuring of object
+const {id}= user
+console.log(id)
+
+// Json = javascript object notation.
+// json exists in array or object format.  
